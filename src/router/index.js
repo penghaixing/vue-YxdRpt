95599f44404c036d779f85969c868f5b8889d79b
seek <seek.peng@alllinktech.com>
2022-02-21 11:54:57 +0800

    up1

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-  { path: '/home', componet: () => import('../Home/home.vue') }
+  { path: '/home', componet: () => import('../views/Home/home.vue') }
 ]
 
 const router = createRouter({
